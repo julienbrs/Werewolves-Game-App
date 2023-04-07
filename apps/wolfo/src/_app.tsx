17be3ef83f668874eaccd367ba08f1a61087c305
Paul Vernin <paul.vernin@gmail.com>
2023-04-07 18:42:11 +0200

    init apps/wolfo

--- apps/wolfo/src/_app.tsx
+++ apps/wolfo/src/_app.tsx
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { registerRootComponent } from "expo";
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+registerRootComponent(App);
 
 const styles = StyleSheet.create({
   container: {
