308d77cd1c68aaea53d4a43aa80ce3c6fecef0ab
Paul Vernin <paul.vernin@gmail.com>
2023-04-18 09:18:50 +0200

    fix change

--- apps/wolfo/src/app/auth/login.tsx
+++ apps/wolfo/src/app/auth/login.tsx
@@ -1,18 +1,18 @@
-import { useMutation } from "@tanstack/react-query";
+import { QueryClient, useMutation } from "@tanstack/react-query";
 import { Button, Input, Text } from "@ui-kitten/components";
 import { Redirect } from "expo-router";
-import * as SecureStore from "expo-secure-store";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Error, NewUser } from "types";
 import { login } from "../../utils/api/user";
 export const Login = () => {
+  const queryClient = new QueryClient();
   const [name, setName] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const { mutate, isSuccess, isError, error } = useMutation<any, Error, NewUser>({
     mutationFn: user => login(user),
     onSuccess: data => {
-      SecureStore.setItemAsync("token", data.token);
+      queryClient.setQueryData(["token"], data.token);
     },
   });
   const handleLogin = () => {
