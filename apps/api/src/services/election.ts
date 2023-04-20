4d843a17484c266528c9e69e8357c0ed8ada59af
Axelle <Axelle.yriarte@grenoble-inp.org>
2023-04-20 21:39:34 +0200

    issue count votes and kill player implemented, not tested

--- apps/api/src/services/election.ts
+++ apps/api/src/services/election.ts
@@ -1,3 +1,4 @@
+import { StatePlayer } from "database";
 export const finishElection = async (
   transaction: any,
   electionId: number,
@@ -16,4 +17,57 @@ export const finishElection = async (
   // cond : le % est > 50% du nombre de joueurs
   // une fois qu'on a un joueur éliminé, il faut le mettre à l'état DEAD et le retirer des chatrooms
   // (uniquement le droit lecteur mais pas d'écriture)
+  const nbPlayers = await transaction.player.findMany({
+    where: { state: StatePlayer.ALIVE },
+  }).length;
+  var voteArray:number[] = new Array(nbPlayers);
+  var killedPlayerId = null;
+  var nbVictims = 0;
+  votes.forEach(vote => {
+     voteArray[vote.targetId] += 1;
+     if (voteArray[vote.targetId] >= (nbPlayers / 2)) {
+      killedPlayerId = vote.targetId;
+      nbVictims++;
+     }
+  });
+  if (killedPlayerId !== null && nbVictims < 2) {
+    await transaction.player.update({
+      where: { userId: killedPlayerId },
+      data: {
+        state: StatePlayer.DEAD,
+      },
+    });
+    const gameId = await transaction.election.findUnique({
+      where: { electionId },
+    }).gameId;
+    const chats = await transaction.game.findUnique({
+      where: { id: gameId },
+      select: {
+        dayChat: true,
+        nightChat: true,
+      },
+    });
+    const dayId = chats.dayChat.id;
+    const nightId = chats.nightChat.id;
+    await transaction.chatroom.update({
+      where: { id: dayId },
+      data: {
+        writers: {
+          disconnect: {
+            playerId: killedPlayerId
+          }
+        }
+      }
+      });
+      await transaction.chatroom.update({
+        where: { id: nightId },
+        data: {
+          writers: {
+            disconnect: {
+              playerId: killedPlayerId
+            }
+          }
+        }
+        });
+  }
 };
