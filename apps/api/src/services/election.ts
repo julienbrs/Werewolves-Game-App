56f15f92dac769509ec1bf1aa2a65e72c5a87f18
Paul Vernin <paul.vernin@gmail.com>
2023-04-20 22:59:06 +0200

    fix typage transaction

--- apps/api/src/services/election.ts
+++ apps/api/src/services/election.ts
@@ -1,34 +1,34 @@
-import { StatePlayer } from "database";
+import { Power, Role, StatePlayer, TransactionType, Vote } from "database";
+
 export const finishElection = async (
-  transaction: any,
+  transaction: TransactionType,
   electionId: number,
-  players: { userId: string }[]
+  players: {
+    state: StatePlayer | null;
+    userId: string;
+    role: Role | null;
+    power: Power | null;
+  }[]
 ) => {
   const votes = await transaction.vote.findMany({
     where: { electionId },
-    select: {
-      voterId: true,
-      targetId: true,
-    },
   });
   console.log(players);
-  console.log(votes);
-  // TODO fonction qui détermine qui est éliminé
-  // cond : le % est > 50% du nombre de joueurs
-  // une fois qu'on a un joueur éliminé, il faut le mettre à l'état DEAD et le retirer des chatrooms
-  // (uniquement le droit lecteur mais pas d'écriture)
-  const nbPlayers = await transaction.player.findMany({
-    where: { state: StatePlayer.ALIVE },
-  }).length;
-  var voteArray:number[] = new Array(nbPlayers);
-  var killedPlayerId = null;
-  var nbVictims = 0;
-  votes.forEach(vote => {
-     voteArray[vote.targetId] += 1;
-     if (voteArray[vote.targetId] >= (nbPlayers / 2)) {
+  const nbPlayers = (
+    await transaction.player.findMany({
+      where: { state: StatePlayer.ALIVE },
+    })
+  ).length;
+
+  const voteArray: number[] = new Array(nbPlayers);
+  const killedPlayerId = null;
+  const nbVictims = 0;
+  votes.forEach((vote: Vote) => {
+    voteArray[vote.targetId] += 1;
+    if (voteArray[vote.targetId] >= nbPlayers / 2) {
       killedPlayerId = vote.targetId;
       nbVictims++;
-     }
+    }
   });
   if (killedPlayerId !== null && nbVictims < 2) {
     await transaction.player.update({
@@ -54,20 +54,20 @@ export const finishElection = async (
       data: {
         writers: {
           disconnect: {
-            playerId: killedPlayerId
-          }
-        }
-      }
-      });
-      await transaction.chatroom.update({
-        where: { id: nightId },
-        data: {
-          writers: {
-            disconnect: {
-              playerId: killedPlayerId
-            }
-          }
-        }
-        });
+            playerId: killedPlayerId,
+          },
+        },
+      },
+    });
+    await transaction.chatroom.update({
+      where: { id: nightId },
+      data: {
+        writers: {
+          disconnect: {
+            playerId: killedPlayerId,
+          },
+        },
+      },
+    });
   }
 };
--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,1 +1,11 @@
+import { Prisma, PrismaClient } from "@prisma/client";
 export * from "@prisma/client";
+
+export type TransactionType = Omit<
+  PrismaClient<
+    Prisma.PrismaClientOptions,
+    never,
+    Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined
+  >,
+  "$connect" | "$disconnect" | "$on" | "$transaction" | "$use"
+>;
