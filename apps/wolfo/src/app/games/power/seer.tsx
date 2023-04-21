fb18577ac06abc66578bec2d69a3318ad18e4225
Manon <andre_manon@icloud.com>
2023-04-22 00:40:27 +0200

    diagramme état-transition final + première implémentation de la page de pouvoir 'voyance' (pas encore testé)

--- apps/wolfo/src/app/games/power/seer.tsx
+++ apps/wolfo/src/app/games/power/seer.tsx
@@ -1,17 +1,52 @@
-import { Text } from "@ui-kitten/components";
+import { useQuery } from "@tanstack/react-query";
+import { Button, Text } from "@ui-kitten/components";
 import { useRouter, useSearchParams } from "expo-router";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Player } from "types";
+import { getPlayers } from "../../../utils/api/player";
+
 const SeerView = () => {
   const router = useRouter();
-  const { userId } = useSearchParams();
-  if (!userId) {
+  const { gameId } = useSearchParams();
+  const {
+    data: players,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["players", gameId],
+    queryFn: () => getPlayers(Number(gameId)),
+  });
+  const [selectedPlayer, setSelectedPlayer] = useState<Player | undefined>();
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
+  const handlePlayerClick = (player: Player) => {
+    setSelectedPlayer(player);
+    setIsButtonDisabled(true);
+  };
+
+  if (isLoading) {
+    return <Text>Loading...</Text>;
+  }
+
+  if (isError) {
     router.back();
   }
 
   return (
     <SafeAreaView>
-      <Text>Spirit | {userId as string}</Text>
+      <Text>Players:</Text>
+      {players.map((player: Player) => (
+        <Button
+          key={player.userId}
+          onPress={() => handlePlayerClick(player)}
+          disabled={isButtonDisabled}
+        >
+          {player.user.name}
+        </Button>
+      ))}
+      <Text>Selected player:</Text>
+      {selectedPlayer ? <Text>{selectedPlayer.power}</Text> : <Text>No player selected</Text>}
     </SafeAreaView>
   );
 };
