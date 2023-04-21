6cf7ba43c19dfe30f3665cd5065c973f0abe5055
julienbrs <julien.bourseau@gmail.com>
2023-04-21 18:56:28 +0200

    types chatroom & cie done

---
+++ packages/types/src/chatroom.ts
@@ -0,0 +1,55 @@
+import { Game } from "./game";
+
+export type Message = {
+  id: number;
+  chatRoomId: number;
+  content: string;
+  authorId: string;
+  gameId: number;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type Reader = {
+  playerId: string;
+  gameId: number;
+  chatRoomId: number;
+};
+
+export type Writer = {
+  playerId: string;
+  gameId: number;
+  chatRoomId: number;
+};
+
+export type ChatRoomType = "night" | "day" | "spirit";
+
+export type NightChatRoom = {
+  id: number;
+  game: Game;
+  chatRoom: ChatRoom;
+};
+
+export type DayChatRoom = {
+  id: number;
+  game: Game;
+  chatRoom: ChatRoom;
+};
+
+export type SpiritChatRoom = {
+  id: number;
+  game: Game;
+  chatRoom: ChatRoom;
+};
+
+export type ChatRoom = {
+  id: number;
+  messages: Message[];
+  readers: Reader[];
+  writers: Writer[];
+  createdAt: Date;
+  updatedAt: Date;
+  nightChat: NightChatRoom[];
+  dayChat: DayChatRoom[];
+  spirit: SpiritChatRoom[];
+};
