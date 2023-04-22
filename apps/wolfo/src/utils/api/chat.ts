60271ed8719e739a4dfb80b8c09a4177436f9769
julienbrs <julien.bourseau@gmail.com>
2023-04-22 12:55:36 +0200

    fix misspell of chatroomS

--- apps/wolfo/src/utils/api/chat.ts
+++ apps/wolfo/src/utils/api/chat.ts
@@ -3,15 +3,15 @@ import api from "./api";
 
 const chatApi = {
   getMessages: async (): Promise<Message[]> => {
-    const { data } = await api.get("/chatroom/:id/messages");
+    const { data } = await api.get("/chatrooms/:id/messages");
     return data;
   },
-  getHistory: async (): Promise<Message[]> => {
-    const { data } = await api.get("/chatroom/:id/history");
+  getHistory: async (chatRoomId: string): Promise<Message[]> => {
+    const { data } = await api.get(`/chatrooms/${chatRoomId}/history`);
     return data;
   },
   createChatroom: async (chatroom: ChatRoom): Promise<ChatRoom> => {
-    const { data } = await api.post("/chatroom", chatroom);
+    const { data } = await api.post("/chatrooms", chatroom);
     return data;
   },
 };
