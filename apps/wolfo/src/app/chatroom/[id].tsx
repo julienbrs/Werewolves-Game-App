62a607039c40d15351b85e039b7653e4e26d6537
julienbrs <julien.bourseau@gmail.com>
2023-04-22 12:54:51 +0200

    Replacing useEffect by useQuery

--- apps/wolfo/src/app/chatroom/[id].tsx
+++ apps/wolfo/src/app/chatroom/[id].tsx
@@ -1,32 +1,69 @@
 import { Button, Text } from "@ui-kitten/components";
 import { useRouter, useSearchParams } from "expo-router";
-import { useEffect, useState } from "react";
 import React from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useQuery } from "react-query";
 import { Message } from "types";
 import { getHistory } from "../../utils/api/chat";
 
 const ChatRoomView = () => {
   const router = useRouter();
   const { id } = useSearchParams(); // id du chatroom
+  /*   
+  Mock of chat history to test the view
+  {
+      id: 1,
+      chatRoomId: 1,
+      content: "Hello",
+      authorId: "John",
+      gameId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id: 2,
+      chatRoomId: 1,
+      content: "HI it's Andrea",
+      authorId: "Andrea",
+      gameId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id: 3,
+      chatRoomId: 1,
+      content: "How are you ?",
+      authorId: "John",
+      gameId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }, */
 
-  const [history, setHistory] = useState([] as Message[]);
+  const {
+    data: history,
+    isLoading,
+    isError,
+  } = useQuery<Message[], Error>({
+    queryKey: ["chatroom", id],
+    queryFn: () => getHistory(typeof id === "string" ? id : ""),
+    enabled: typeof id === "string",
+  });
 
-  useEffect(() => {
-    const getChatHistory = async () => {
-      console.log("getChatHistory");
-      const chatHistory = await getHistory();
-      setHistory(chatHistory);
-    };
-    getChatHistory();
-  }, []);
+  if (isLoading) {
+    return <Text>Loading chat history...</Text>;
+  }
+
+  if (isError) {
+    return <Text>Error loading chat history</Text>;
+  }
 
   return (
     <SafeAreaView>
       <Text>ChatRoom | {Number(id)}</Text>
-      {history.map((message, index) => (
-        <Text key={index}>{message.content}</Text>
-      ))}
+      {history &&
+        history.map((message: Message, index: number) => (
+          <Text key={index}>{message.content}</Text>
+        ))}
       <Button onPress={() => router.back()}>Go Back</Button>
     </SafeAreaView>
   );
