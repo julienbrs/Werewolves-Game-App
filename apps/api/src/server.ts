53f3c3245f7ed5403af37668d5f7b5eff0573dde
julienbrs <julien.bourseau@gmail.com>
2023-04-23 21:07:43 +0200

    init branch

---
+++ apps/api/src/server.ts
@@ -0,0 +1,31 @@
+import { Server } from "socket.io";
+import { app } from "./app";
+import prisma from "./prisma";
+const IP = process.env.IP || "localhost";
+const PORT = process.env.PORT || 3000;
+
+const serv = app.listen(PORT, () => console.log(`Listening on http://${IP}:${PORT}`));
+
+// Websocket server
+const io = new Server(serv);
+
+// Listen for incoming socket connections
+io.on("connection", socket => {
+  console.log("a user is connected to the chat");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("messagePosted", message => {
+    console.log("messagePosted", message);
+    prisma.message.create({
+      data: {
+        content: message.text,
+        chatRoomId: message.chatRoomId,
+        authorId: message.authorId,
+        gameId: message.gameId,
+      },
+    });
+  });
+});
