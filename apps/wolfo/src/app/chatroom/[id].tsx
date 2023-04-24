229886b281f741d073ff007c8fd70ae87b2e704d
julienbrs <julien.bourseau@gmail.com>
2023-04-24 16:31:17 +0200

    chat ok, no moer infinite loop

--- apps/wolfo/src/app/chatroom/[id].tsx
+++ apps/wolfo/src/app/chatroom/[id].tsx
@@ -2,10 +2,9 @@ import { Button, Input, Text } from "@ui-kitten/components";
 import { useRouter, useSearchParams } from "expo-router";
 import React, { useEffect, useState } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import io from "socket.io-client";
-import { getMessages } from "../../utils/api/chat";
-
+import io, { Socket } from "socket.io-client";
 import { Message, NewMessage } from "types";
+import { getMessages } from "../../utils/api/chat";
 
 const IP = process.env.IP || "localhost";
 const PORT = process.env.PORT || 3000;
@@ -15,8 +14,23 @@ const ChatRoomView = () => {
   const [messageInput, setMessageInput] = useState<string>("");
   const [messages, setMessages] = useState<Message[]>([]);
   const router = useRouter();
-  const { id, gameId, userId } = useSearchParams(); // id du chatroom
-  const socket = io(socketEndpoint);
+  const { id } = useSearchParams(); // id du chatroom
+  const userId = "512daa49-a394-4afc-99b3-1e6a0e7daf88";
+  const gameId = "1";
+
+  // Create a state for the socket instance
+  const [socket, setSocket] = useState<Socket | null>(null);
+  useEffect(() => {
+    // Instantiate the socket inside the useEffect
+    const newSocket = io(socketEndpoint);
+    setSocket(newSocket);
+
+    return () => {
+      if (newSocket) {
+        newSocket.disconnect();
+      }
+    };
+  }, []);
 
   useEffect(() => {
     const fetchMessages = async () => {
@@ -30,31 +44,31 @@ const ChatRoomView = () => {
 
     fetchMessages();
 
-    socket.on("connect", () => {
-      console.log("Connected to socket server");
-      socket.emit("joinChatRoom", { chatRoomId: id, userId });
-    });
+    if (socket) {
+      socket.on("connect", () => {
+        console.log("Connected to socket server");
+        socket.emit("joinChatRoom", { chatRoomId: id, userId });
+      });
 
-    socket.on("disconnect", () => {
-      console.log("Disconnected from socket server");
-    });
+      socket.on("disconnect", () => {
+        console.log("Disconnected from socket server");
+      });
 
-    const handleNewMessage = (msg: Message) => {
-      setMessages(prevMessages => [...prevMessages, msg]);
-    };
+      const handleNewMessage = (msg: Message) => {
+        setMessages(prevMessages => [...prevMessages, msg]);
+      };
 
-    socket.on("newMessage", handleNewMessage);
+      socket.on("newMessage", handleNewMessage);
 
-    return () => {
-      socket.off("newMessage", handleNewMessage); // désactive l'écoute de l'événement lors du nettoyage de l'effet.
-      socket.disconnect();
-    };
-  }, [id, userId, socket, messages]);
+      return () => {
+        socket.off("newMessage", handleNewMessage); // désactive l'écoute de l'événement lors du nettoyage de l'effet.
+        socket.disconnect();
+      };
+    }
+  }, [id, userId, socket]);
 
   const sendMessage = () => {
     try {
-      const userId = "512daa49-a394-4afc-99b3-1e6a0e7daf88";
-      const gameId = "1";
       console.log(
         `Sending message: ${messageInput} from ${userId} in game ${gameId} in chatroom ${id}`
       );
