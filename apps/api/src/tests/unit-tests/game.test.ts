01f9c4c6c729ac02dd7cfb6701584353146ebe4a
Axelle <Axelle.yriarte@grenoble-inp.org>
2023-04-26 16:53:38 +0200

    lint

--- apps/api/src/tests/unit-tests/game.test.ts
+++ apps/api/src/tests/unit-tests/game.test.ts
@@ -148,21 +148,21 @@ describe("Scénario création de partie -> ajout de joueurs -> lancement", () =>
 
   describe("Lancement puis suppression de la game", () => {
     test("startGame then delete /api/games/:id", async () => {
-  //
-  //  // On lance la partie qui est pleine
-  //  await startGame(gameId);
-  //  const repGetGame = await request
-  //  .get(`/api/games/${gameId}`)
-  //  .set("Authorization", `Bearer ${token}`)
-  //  .set("Content-Type", "application/json");
-  //  expect(repGetGame.body.players.length).toBe(5);
-  //  console.log(repGetGame.body.players);
-  //  console.log(repGetGame.body);
-    //on supprime la game
-    await request
-    .delete(`/api/games/${gameId}/delete`)
-    .set("Authorization", `Bearer ${token}`)
-    .set("Content-Type", "application/json");
+      //
+      //  // On lance la partie qui est pleine
+      //  await startGame(gameId);
+      //  const repGetGame = await request
+      //  .get(`/api/games/${gameId}`)
+      //  .set("Authorization", `Bearer ${token}`)
+      //  .set("Content-Type", "application/json");
+      //  expect(repGetGame.body.players.length).toBe(5);
+      //  console.log(repGetGame.body.players);
+      //  console.log(repGetGame.body);
+      //on supprime la game
+      await request
+        .delete(`/api/games/${gameId}/delete`)
+        .set("Authorization", `Bearer ${token}`)
+        .set("Content-Type", "application/json");
     });
   });
 });
