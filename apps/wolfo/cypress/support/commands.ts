438cfc53ea1a3648d5c1270904fe2a3ccbde20d7
Manon <andre_manon@icloud.com>
2023-05-12 00:56:57 +0200

    tests cypress front authentification, poursuivre debug + push spirit view

--- apps/wolfo/cypress/support/commands.ts
+++ apps/wolfo/cypress/support/commands.ts
@@ -34,4 +34,77 @@
 //       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
 //     }
 //   }
+
 // }
+
+export { };
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * Custom command to intercept GET endpoint for tags
+       * @example cy.getTags()
+       */
+      getUsers(): Chainable<void>;
+      /**
+       * Custom command to intercept POST endpoint for tags
+       * @example cy.postTags()
+       */
+      getMe(): Chainable<void>;
+      /**
+       * Custom command to intercept DELETE endpoint for tags
+       * @example cy.deleteTags()
+       */
+      createUser(): Chainable<void>;
+      /**
+       * Custom command to intercept PUT endpoint for bookmarks
+       * @example cy.putTag()
+       */
+      deleteUser(): Chainable<void>;
+      /**
+       * Custom command to intercept GET endpoint for bookmarks
+       * @example cy.getBookmarks()
+       */
+      updateUser(): Chainable<void>;
+      /**
+       * Custom command to intercept POST endpoint for bookmarks
+       * @example cy.postBookmarks()
+       */
+      login(): Chainable<void>;
+    }
+  }
+}
+
+Cypress.Commands.add('getTags', () => {
+  cy.intercept('GET','https://cawrest.osc-fr1.scalingo.io/bmt/verninp/tags',).as('getTags');
+});
+
+Cypress.Commands.add('postTag', () => {
+  cy.intercept('POST','https://cawrest.osc-fr1.scalingo.io/bmt/verninp/tags',).as('postTag');
+});
+
+Cypress.Commands.add('deleteTag', () => {
+  cy.intercept('DELETE','https://cawrest.osc-fr1.scalingo.io/bmt/verninp/tags/*',).as('deleteTag');
+});
+
+Cypress.Commands.add('putTag', () => {
+  cy.intercept('PUT','https://cawrest.osc-fr1.scalingo.io/bmt/verninp/tags/*',).as('putTag');
+});
+
+
+Cypress.Commands.add('getBookmarks', () => {
+  cy.intercept('GET','https://cawrest.osc-fr1.scalingo.io/bmt/verninp/bookmarks',).as('getBookmarks');
+});
+
+Cypress.Commands.add('postBookmark', () => {
+  cy.intercept('POST','https://cawrest.osc-fr1.scalingo.io/bmt/verninp/bookmarks',).as('postBookmark');
+});
+
+Cypress.Commands.add('deleteBookmark', () => {
+  cy.intercept('DELETE','https://cawrest.osc-fr1.scalingo.io/bmt/verninp/bookmarks/*',).as('deleteBookmark');
+});
+
+Cypress.Commands.add('putBookmark', () => {
+  cy.intercept('PUT','https://cawrest.osc-fr1.scalingo.io/bmt/verninp/bookmarks/*',).as('putBookmark');
+});
