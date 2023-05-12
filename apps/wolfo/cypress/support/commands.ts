b4e62f95849199fb2446636f99162e452f096a37
Manon <andre_manon@icloud.com>
2023-05-12 10:43:14 +0200

    modif command.ts

--- apps/wolfo/cypress/support/commands.ts
+++ apps/wolfo/cypress/support/commands.ts
@@ -37,7 +37,7 @@
 
 // }
 
-export { };
+export {};
 
 declare global {
   namespace Cypress {
@@ -76,35 +76,42 @@ declare global {
   }
 }
 
-Cypress.Commands.add('getTags', () => {
-  cy.intercept('GET','https://cawrest.osc-fr1.scalingo.io/bmt/verninp/tags',).as('getTags');
+Cypress.Commands.add("getTags", () => {
+  cy.intercept("GET", "https://cawrest.osc-fr1.scalingo.io/bmt/verninp/tags").as("getTags");
 });
 
-Cypress.Commands.add('postTag', () => {
-  cy.intercept('POST','https://cawrest.osc-fr1.scalingo.io/bmt/verninp/tags',).as('postTag');
+Cypress.Commands.add("postTag", () => {
+  cy.intercept("POST", "https://cawrest.osc-fr1.scalingo.io/bmt/verninp/tags").as("postTag");
 });
 
-Cypress.Commands.add('deleteTag', () => {
-  cy.intercept('DELETE','https://cawrest.osc-fr1.scalingo.io/bmt/verninp/tags/*',).as('deleteTag');
+Cypress.Commands.add("deleteTag", () => {
+  cy.intercept("DELETE", "https://cawrest.osc-fr1.scalingo.io/bmt/verninp/tags/*").as("deleteTag");
 });
 
-Cypress.Commands.add('putTag', () => {
-  cy.intercept('PUT','https://cawrest.osc-fr1.scalingo.io/bmt/verninp/tags/*',).as('putTag');
+Cypress.Commands.add("putTag", () => {
+  cy.intercept("PUT", "https://cawrest.osc-fr1.scalingo.io/bmt/verninp/tags/*").as("putTag");
 });
 
-
-Cypress.Commands.add('getBookmarks', () => {
-  cy.intercept('GET','https://cawrest.osc-fr1.scalingo.io/bmt/verninp/bookmarks',).as('getBookmarks');
+Cypress.Commands.add("getBookmarks", () => {
+  cy.intercept("GET", "https://cawrest.osc-fr1.scalingo.io/bmt/verninp/bookmarks").as(
+    "getBookmarks"
+  );
 });
 
-Cypress.Commands.add('postBookmark', () => {
-  cy.intercept('POST','https://cawrest.osc-fr1.scalingo.io/bmt/verninp/bookmarks',).as('postBookmark');
+Cypress.Commands.add("postBookmark", () => {
+  cy.intercept("POST", "https://cawrest.osc-fr1.scalingo.io/bmt/verninp/bookmarks").as(
+    "postBookmark"
+  );
 });
 
-Cypress.Commands.add('deleteBookmark', () => {
-  cy.intercept('DELETE','https://cawrest.osc-fr1.scalingo.io/bmt/verninp/bookmarks/*',).as('deleteBookmark');
+Cypress.Commands.add("deleteBookmark", () => {
+  cy.intercept("DELETE", "https://cawrest.osc-fr1.scalingo.io/bmt/verninp/bookmarks/*").as(
+    "deleteBookmark"
+  );
 });
 
-Cypress.Commands.add('putBookmark', () => {
-  cy.intercept('PUT','https://cawrest.osc-fr1.scalingo.io/bmt/verninp/bookmarks/*',).as('putBookmark');
+Cypress.Commands.add("putBookmark", () => {
+  cy.intercept("PUT", "https://cawrest.osc-fr1.scalingo.io/bmt/verninp/bookmarks/*").as(
+    "putBookmark"
+  );
 });
