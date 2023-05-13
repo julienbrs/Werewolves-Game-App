06e214d3e5de33fd31739ff3dabdef3a609e0874
julienbrs <julien.bourseau@gmail.com>
2023-05-14 00:59:25 +0200

    first part auth

--- apps/wolfo/src/app/(auth)/auth.tsx
+++ apps/wolfo/src/app/(auth)/auth.tsx
@@ -1,7 +1,6 @@
 import { Tab, TabView } from "@ui-kitten/components";
 import React, { useState } from "react";
-import { ImageBackground, StyleSheet, View } from "react-native";
-import imgBackground from "../../../assets/homepage_day.png";
+import { StyleSheet, View } from "react-native";
 import { Login } from "../../components/auth/login";
 import { Register } from "../../components/auth/register";
 
@@ -9,18 +8,16 @@ const Auth = () => {
   const [tabIndex, setTabIndex] = useState<number>(0);
   return (
     <View style={styles.container}>
-      <ImageBackground source={imgBackground} style={styles.image}>
-        <View style={styles.tabViewWrapper}>
-          <TabView selectedIndex={tabIndex} onSelect={setTabIndex} style={styles.tabView}>
-            <Tab title="Login" style={styles.title}>
-              <Login />
-            </Tab>
-            <Tab title="Register">
-              <Register />
-            </Tab>
-          </TabView>
-        </View>
-      </ImageBackground>
+      <View style={styles.tabViewWrapper}>
+        <TabView selectedIndex={tabIndex} onSelect={setTabIndex} style={styles.tabView}>
+          <Tab title="Login" style={styles.title}>
+            <Login />
+          </Tab>
+          <Tab title="Register">
+            <Register />
+          </Tab>
+        </TabView>
+      </View>
     </View>
   );
 };
@@ -29,12 +26,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
-    overflow: "hidden",
-  },
-  image: {
-    flex: 1,
-    resizeMode: "cover",
     justifyContent: "center",
+    alignContent: "center",
+    overflow: "hidden",
+    backgroundColor: "#141313",
   },
   tabViewWrapper: {
     width: "70%",
@@ -48,6 +43,14 @@ const styles = StyleSheet.create({
   },
   title: {
     paddingVertical: 5,
+  },
+  textButton: {
+    fontFamily: "MontserratBold",
+    color: "#141313",
+  },
+  text: {
+    fontFamily: "Montserrat",
+    color: "#C38100",
   },
 });
 
