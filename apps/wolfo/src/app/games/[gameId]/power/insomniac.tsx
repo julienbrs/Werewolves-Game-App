0aa10f0b6556456578ffeefeccea7addd1b788aa
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-05-15 18:22:50 +0200

    updating import

--- apps/wolfo/src/app/games/[gameId]/power/insomniac.tsx
+++ apps/wolfo/src/app/games/[gameId]/power/insomniac.tsx
@@ -2,7 +2,7 @@ import { Text } from "@ui-kitten/components";
 import { Stack, useRouter } from "expo-router";
 import React, { useContext } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { AuthContext } from "src/components/context/tokenContext";
+import { AuthContext } from "../../../../components/context/tokenContext";
 const InsomniacView = () => {
   const router = useRouter();
   const { id: userId } = useContext(AuthContext);
