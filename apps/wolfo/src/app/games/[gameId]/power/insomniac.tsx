dd64a38d6aec1a21f2c5f058ecdfa9eb96a94bc4
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-05-15 18:20:06 +0200

    insomniac not clickable fix

--- apps/wolfo/src/app/games/[gameId]/power/insomniac.tsx
+++ apps/wolfo/src/app/games/[gameId]/power/insomniac.tsx
@@ -1,10 +1,11 @@
 import { Text } from "@ui-kitten/components";
-import { Stack, useRouter, useSearchParams } from "expo-router";
-import React from "react";
+import { Stack, useRouter } from "expo-router";
+import React, { useContext } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { AuthContext } from "src/components/context/tokenContext";
 const InsomniacView = () => {
   const router = useRouter();
-  const { userId } = useSearchParams();
+  const { id: userId } = useContext(AuthContext);
   if (!userId) {
     router.back();
   }
