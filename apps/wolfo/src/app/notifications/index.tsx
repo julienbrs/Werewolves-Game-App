7a9abdc2350a5328a894c54572e38dc17c78bd18
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-05-15 17:07:59 +0200

    notifications ui

--- apps/wolfo/src/app/notifications/index.tsx
+++ apps/wolfo/src/app/notifications/index.tsx
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
-import { Divider, List, ListItem } from "@ui-kitten/components";
+import { Divider, ListItem } from "@ui-kitten/components";
 import { useRouter } from "expo-router";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Notification } from "types";
 import Loading from "../../components/loading";
@@ -32,9 +33,14 @@ const Auth = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.h2}>Notifications</Text>
+      <View style={styles.wrapperTitle}>
+        <View style={styles.line}>
+          <Text>{""}</Text>
+        </View>
+        <Text style={styles.h2}>Notifications</Text>
+      </View>
       <View style={styles.mainWrapper}>
-        <List
+        <FlatList
           data={data}
           renderItem={listNotification}
           ItemSeparatorComponent={Divider}
@@ -48,6 +54,22 @@ const Auth = () => {
 };
 
 const styles = StyleSheet.create({
+  wrapperTitle: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    marginTop: "5%",
+    marginBottom: 40,
+  },
+  line: {
+    position: "relative",
+    borderColor: "#C38100",
+    height: 0,
+    borderBottomWidth: 2,
+    width: "80%",
+  },
   container: {
     flex: 1,
     backgroundColor: "#141313",
@@ -60,14 +82,16 @@ const styles = StyleSheet.create({
     borderColor: "#C38100",
     borderWidth: 2,
     borderRadius: 10,
-    paddingHorizontal: 25,
-    paddingTop: 20,
+    paddingHorizontal: 20,
+    paddingTop: 15,
     paddingBottom: 10,
     display: "flex",
     flexDirection: "column",
     justifyContent: "space-between",
     alignItems: "center",
     width: "80%",
+    height: "70%",
+    marginBottom: 30,
     backgroundColor: "white",
   },
   h2: {
@@ -75,6 +99,8 @@ const styles = StyleSheet.create({
     fontFamily: "Voyage",
     fontSize: 37,
     color: "#C38100",
+    paddingHorizontal: 10,
+    marginTop: -25,
     marginBottom: 20,
   },
 });
